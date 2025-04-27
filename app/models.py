a284fee651fac18f3738fd3358ad629e1f163576
Adi Budhavaram <avbudhavaram@gmail.com>
2025-04-27 13:20:19 +0800

    Added database model for expenses

--- app/models.py
+++ app/models.py
@@ -8,7 +8,16 @@ class User(db.Model, UserMixin):
     password_hash = db.Column(db.String(128), nullable=False)
    
 
+from datetime import date
 
+class Expense(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    month = db.Column(db.String(7), nullable=False)
+    category = db.Column(db.String(50), nullable=False)
+    amount = db.Column(db.Float, nullable=False)
+    city = db.Column(db.String(50), nullable=False)
+    notes = db.Column(db.String(200))
 
 
 
